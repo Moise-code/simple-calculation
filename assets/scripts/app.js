8d2468e2a02692ffaab613356d03618e81e88915
Moise-code <moisewowe@gmail.com>
2023-01-11 00:41:23 -0800

    adding the add function to make the addition of two numbers

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+// we are going to declare the starting point
+const defaultValue = 0;
+let currentValue = defaultValue;
+let resultDescription = ``;
+
+// now we are going to create the add function to add to values the current result and the value user entered.
+
+const add = () =>{
+    resultDescription = `the current value ${currentValue} + the value user entered ${inputNumber.value}`;
+
+}
