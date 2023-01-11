06b8d8363a9ab3742a7390cf15868e32bb8d6d20
Moise-code <moisewowe@gmail.com>
2023-01-11 00:43:28 -0800

    adding the value for the current value to take the current value and adds the entered one to be stored in the current value.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,6 @@ let resultDescription = ``;
 // now we are going to create the add function to add to values the current result and the value user entered.
 
 const add = () =>{
-    resultDescription = `the current value ${currentValue} + the value user entered ${inputNumber.value}`;
-
+    resultDescription = `the current value ${currentValue} + the value user entered ${inputNumber.value.trim}`;
+    currentValue = currentValue + parseInt(inputNumber.value.trim());
 }
