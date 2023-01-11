ccc6870af10beaadd600254bc461c00164a08886
Moise-code <moisewowe@gmail.com>
2023-01-11 00:36:13 -0800

    adding the function to take result and description and allow the assignment of new values to it.

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -9,4 +9,8 @@ const desc = document.querySelector("#current-calculation");
 const result = document.querySelector("#currrent-result");
 
 
-// 
+// adding the out put function to be called in the app.js
+const outputResult = (results, description) =>{
+    result.textContent = results;
+    desc.textContent= description;
+}
