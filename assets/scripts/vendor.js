03ca9523114d9301e3250ff87474982a75f06702
Moise-code <moisewowe@gmail.com>
2023-01-11 00:32:45 -0800

    declare the buttons to be used

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,12 @@
+// getting the buttons to use.
+
+const inputNumber = document.querySelector('#input-number');
+const addBtn = document.querySelector("#btn-add");
+const minusBtn = document.querySelector("#btn-minus");
+const multiplyBtn = document.querySelector("#btn-multiply");
+const divideBtn = document.querySelector("#btn-divide");
+const desc = document.querySelector("#current-calculation");
+const result = document.querySelector("#currrent-result");
+
+
+// 
