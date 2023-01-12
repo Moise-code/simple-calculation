c42c41c5b92fcadc513ca74b9ed4acba1a4ae4df
Moise-code <moisewowe@gmail.com>
2023-01-11 23:43:27 -0800

    refactor the codes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,67 +1,88 @@
 // we are going to declare the starting point
 const defaultValue = 0;
 let currentValue = defaultValue;
-
 let resultDescription = ``;
-
+let logEntries = [];
 
 // function to get input from input field
 
-const getUserNumberInput = () =>{
-    return parseInt(inputNumber.value)
-}
+const getUserNumberInput = () => {
+  return parseInt(inputNumber.value);
+};
 
 // function to generates and writes calculation log
 
-const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) =>{
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
-    outputResult(currentValue, calcDescription); // from vendor file.
-}
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentValue, calcDescription); // from vendor file.
+};
+
+// let create write to log function
+
+const writeToLog =
+  (operationaIdentifier, prevResult, operationNumber, newResult) =>{
+    const logEntry = {
+        sign: operationaIdentifier,
+        prevNumber: prevResult,
+        fromUser: operationNumber,
+        result: newResult,
+      };
+      logEntries.push(logEntry);
+      console.log(logEntries);
+  };
 
 // function to add two valuesd
-const add = () =>{
-    const enteredNumber = getUserNumberInput();
-    currentValue += parseInt(enteredNumber);
-    inputNumber.value = '';
-    createAndWriteOutput('+',currentValue, enteredNumber)
-}
+const add = () => {
+  const enteredNumber = getUserNumberInput();
+  initialValue = currentValue;
+  currentValue += parseInt(enteredNumber);
+  inputNumber.value = "";
+  createAndWriteOutput("+", currentValue, enteredNumber);
+  writeToLog('Addition',initialValue,enteredNumber,currentValue)
+};
 
 // add event listerner to add button
 
-addBtn.addEventListener('click', add);
+addBtn.addEventListener("click", add);
 
-const minus = () =>{
-    const enteredNumber = getUserNumberInput();
-    currentValue -=  parseInt(enteredNumber);
-    inputNumber.value = '';
-    createAndWriteOutput('-', currentValue, enteredNumber)
-}
+const minus = () => {
+  const enteredNumber = getUserNumberInput();
+  initialValue = currentValue;
+  currentValue -= parseInt(enteredNumber);
+  inputNumber.value = "";
+  createAndWriteOutput("-", currentValue, enteredNumber);
+  writeToLog('SUBSTRACTION', initialValue, enteredNumber, currentValue)
+};
 
 // add eventListerner to minus button
 
-minusBtn.addEventListener('click', minus);
+minusBtn.addEventListener("click", minus);
 
 // multiply function
-const multiply = () =>{
-    const enteredNumber = getUserNumberInput();
-    currentValue *= parseInt(enteredNumber);
-    inputNumber.value = '';
-    createAndWriteOutput('*', currentValue, enteredNumber)
-}
+const multiply = () => {
+  const enteredNumber = getUserNumberInput();
+  initialValue = currentValue;
+  currentValue *= parseInt(enteredNumber);
+  inputNumber.value = "";
+  createAndWriteOutput("*", currentValue, enteredNumber);
+  writeToLog('Multiply',initialValue,enteredNumber,currentValue)
+};
 
 // add eventlisterner to multiply button
 
-multiplyBtn.addEventListener('click', multiply);
+multiplyBtn.addEventListener("click", multiply);
 
 // adding the devide function to devide two numbers
 
-const devide = () =>{
-    const enteredNumber = getUserNumberInput();
-    currentValue /= parseInt(enteredNumber);
-    inputNumber.value = '';
-    createAndWriteOutput('/',currentValue,enteredNumber)
-}
+const devide = () => {
+  const enteredNumber = getUserNumberInput();
+  initialValue = currentValue;
+  currentValue /= parseInt(enteredNumber);
+  inputNumber.value = "";
+  createAndWriteOutput("/", currentValue, enteredNumber);
+  writeToLog('DIVIDE',initialValue,enteredNumber,currentValue)
+};
 
 // adding the eventListerner to the devide button.
 
-divideBtn.addEventListener('click', devide);
+divideBtn.addEventListener("click", devide);
